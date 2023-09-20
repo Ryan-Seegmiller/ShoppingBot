7096cd72a1d89f9467b46f6a94900ebfde36816a
TBoneHolmes <s239054@students.aie.edu>
2023-09-20 09:54:05 -0700

    Moved enums and made new functions in GameManager
    Moved item enums to their own ItemEnums script and added some new functions to GameManager such as 'ItemName'

---
+++ Assets/Item/Scripts/ItemEnums.cs
@@ -0,0 +1,25 @@
+//Item id enum
+public enum itemID
+{
+    //Food
+    apple,
+    banana,
+    strawberry,
+    pineapple,
+    //Clothes
+    shoe,
+    hat,
+    glasses,
+    //Tools
+    screwdriver,
+    hammer,
+    wrench
+};
+
+//Item category enum
+public enum itemCategory
+{
+    food,
+    clothes,
+    tools
+};
