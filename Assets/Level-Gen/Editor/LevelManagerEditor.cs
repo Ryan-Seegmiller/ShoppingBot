fbb5abde93992e6096586acf87d15f99d80a54de
CondorHalcon <judahhohawi@outlook.com>
2023-09-19 17:56:22 -0700

    LevelManager Editor
    You can now instance and delete the mall separately from the elevator.

---
+++ Assets/Level-Gen/Editor/LevelManagerEditor.cs
@@ -0,0 +1,44 @@
+using Level;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Level;
+
+
+[CustomEditor(typeof(LevelManager))]
+public class LevelManagerEditor : Editor
+{
+    LevelManager manager;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        manager = (LevelManager)target;
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Instance Full Mall"))
+        {
+            manager.InstanceElevatorShaft();
+            manager.InstanceMall();
+        }
+        if (GUILayout.Button("Instance Elevator"))
+        {
+            manager.InstanceElevatorShaft();
+        }
+        if (GUILayout.Button("Instance Mall"))
+        {
+            manager.InstanceMall();
+        }
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Destroy Full Level"))
+        {
+            manager.DeleteLevel(true);
+        }
+        if (GUILayout.Button("Destroy Level"))
+        {
+            manager.DeleteLevel();
+        }
+    }
+}
--- Assets/Level-Gen/Scripts/LevelManager.cs
+++ Assets/Level-Gen/Scripts/LevelManager.cs
@@ -9,55 +9,70 @@ namespace Level
         [SerializeField] internal MapData mapData;
 
         #region Level Instantiation
-        public void ElevatorShaft()
+        public void InstanceElevatorShaft()
         {
-
+            for (int i = 0; i < MapData.maxFloors; i++)
+            {
+                if (i == 0)
+                {
+                    Instantiate(mapData.elevator, Vector3.zero, Quaternion.identity, transform);
+                } else
+                {
+                    Instantiate(mapData.elevatorShaft, new Vector3(0, i * MapData.tileHeight, 0), Quaternion.identity, transform);
+                }
+            }
         }
-        public void DeleteLevel()
+        public void DeleteLevel(bool includeShaft = false)
         {
-
+            if (!includeShaft && transform.childCount <= MapData.maxFloors) { return; }
+            int lastIndex = (!includeShaft) ? MapData.maxFloors: 0;
+            for (int i = transform.childCount - 1; i >= lastIndex; i--)
+            {
+                DestroyImmediate(transform.GetChild(i).gameObject);
+            }
         }
         public void InstanceMall()
         {
             int floorRamp = (mapData.floorNum > 1) ? Random.Range(1, mapData.mapSize-1) : 0;
-            for (int i = 0; i < mapData.floorNum; i++)
+            for (int i = 0; i < mapData.floorNum; i++) // loops through the floors
             {
-                for (int j = 0; j < mapData.mapSize; j++)
+                for (int j = 0; j < mapData.mapSize; j++) // loops through the x axis of a single floor
                 {
-                    for (int k = 0; k < mapData.mapSize; k++)
+                    for (int k = 0; k < mapData.mapSize; k++) // loops through the z axis of a single floor
                     {
-                        GameObject go;
+                        // SPAWN TILES
                         bool specialTile = (j == 0 && k == 0) || (j == floorRamp && k == floorRamp);
                         if (j == 0 && k == 0)
                         {
-                            go = Instantiate(mapData.elevatorTile, new Vector3(j * MapData.tileSize, i * 5, k * MapData.tileSize), Quaternion.identity, transform);
+                            // Do nothing. This is the elevator shaft
                         } else if (j == floorRamp && k == floorRamp && mapData.floorNum - i > 1)
                         {
                             int randRot = 90 * Random.Range(0, 4);
-                            go = Instantiate(mapData.ramp, new Vector3(j * MapData.tileSize, i * 5, k * MapData.tileSize), Quaternion.Euler(0, randRot, 0), transform);
+                            Instantiate(mapData.ramp, new Vector3(j * MapData.tileSize, i * MapData.tileHeight, k * MapData.tileSize), Quaternion.Euler(0, randRot, 0), transform);
                         }
                         else if (!specialTile)
                         {
                             int mallTileIndex = (mapData.mallTiles.Length <= 1) ? 0 : Random.Range(0, mapData.mallTiles.Length);
                             int randRot = 90 * Random.Range(0, 4);
-                            go = Instantiate(mapData.mallTiles[mallTileIndex], new Vector3(j * MapData.tileSize, i * 5, k * MapData.tileSize), Quaternion.Euler(0, randRot, 0), transform);
+                            Instantiate(mapData.mallTiles[mallTileIndex], new Vector3(j * MapData.tileSize, i * MapData.tileHeight, k * MapData.tileSize), Quaternion.Euler(0, randRot, 0), transform);
                         }
-                        // spawn mall exterior walls
-                        if (j == 0)
+
+                        // SPAWN EXTERIOR MALL WALLS
+                        if (j == 0 && k != 0)
                         {
-                            GameObject wall = Instantiate(mapData.wall, new Vector3(j * MapData.tileSize, i * 5, k * MapData.tileSize), Quaternion.identity, transform);
+                            Instantiate(mapData.wall, new Vector3(j * MapData.tileSize, i * MapData.tileHeight, k * MapData.tileSize), Quaternion.identity, transform);
                         }
                         else if (j == mapData.mapSize - 1)
                         {
-                            GameObject wall = Instantiate(mapData.wall, new Vector3(j * MapData.tileSize, i * 5, k * MapData.tileSize), Quaternion.Euler(0, 180, 0), transform);
+                            Instantiate(mapData.wall, new Vector3(j * MapData.tileSize, i * MapData.tileHeight, k * MapData.tileSize), Quaternion.Euler(0, 180, 0), transform);
                         }
-                        if (k == 0)
+                        if (k == 0 && j != 0)
                         {
-                            GameObject wall = Instantiate(mapData.wall, new Vector3(j * MapData.tileSize, i * 5, k * MapData.tileSize), Quaternion.Euler(0, -90, 0), transform);
+                            Instantiate(mapData.wall, new Vector3(j * MapData.tileSize, i * MapData.tileHeight, k * MapData.tileSize), Quaternion.Euler(0, -90, 0), transform);
                         }
                         else if (k == mapData.mapSize - 1)
                         {
-                            GameObject wall = Instantiate(mapData.wall, new Vector3(j * MapData.tileSize, i * 5, k * MapData.tileSize), Quaternion.Euler(0, 90, 0), transform);
+                            Instantiate(mapData.wall, new Vector3(j * MapData.tileSize, i * MapData.tileHeight, k * MapData.tileSize), Quaternion.Euler(0, 90, 0), transform);
                         }
                     }
                 }
@@ -67,6 +82,7 @@ namespace Level
 
         private void Start()
         {
+            InstanceElevatorShaft();
             InstanceMall();
         }
     }
