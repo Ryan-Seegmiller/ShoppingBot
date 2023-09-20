63b3a27204b6725de3e1062f0c918edeaaddc4c6
studentPun <s239053@students.aie.edu>
2023-09-20 12:54:53 -0700

    score v1

--- Assets/UIandMore/ScoreCalc.cs
+++ Assets/UIandMore/ScoreCalc.cs
@@ -8,6 +8,8 @@ public class ScoreCalc : MonoBehaviour
 
     public int scoreVal;
 
+    int[] testValues1 = new int[] {6000, 12000, 24000};
+
     void Start()
     {
         //Singleton
@@ -19,10 +21,26 @@ public class ScoreCalc : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        scoreVal = 0;
     }
 
     public int GetScore()
     {
+        CalcScore();
         return scoreVal;
+    }
+    public void CalcScore()
+    {
+        scoreVal = 0;
+        int t = TimeCalc.instance.timer;
+        int j = testValues1.Length;
+        for(int i = 0; i < testValues1.Length; i++)
+        {
+            if(t < testValues1[i])
+            {
+                scoreVal += 10 * j;
+            }
+            j--;
+        }
     }
 }
