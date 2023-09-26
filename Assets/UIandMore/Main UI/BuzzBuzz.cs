6189da8401084584b6d5b96a4f24f331c535fa98
studentPun <s239053@students.aie.edu>
2023-09-25 17:55:33 -0700

    Merge branch 'UI'

---
+++ Assets/UIandMore/Main UI/BuzzBuzz.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuzzBuzz : MonoBehaviour
+{
+    [SerializeField] Sprite sp1;
+    [SerializeField] Sprite sp2;
+
+    [SerializeField] Image display;
+
+    bool first = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(Buzz());
+    }
+
+    IEnumerator Buzz()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            if (first)
+            {
+                display.sprite = sp2;
+                first = false;
+            }
+            else
+            {
+                display.sprite = sp1;
+                first = true;
+            }
+        }
+    }
+}
---
+++ Assets/UIandMore/Main UI/NumberDisplays.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NumberDisplays : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI moneyAmt;
+    [SerializeField] TextMeshProUGUI timer;
+
+    private void Update()
+    {
+        UpdateDisplay();
+    }
+
+    public void UpdateDisplay()
+    {
+        moneyAmt.text = "$" + GameManager.Instance.cash.ToString();
+        timer.text = TimeCalc.instance.GetTimeString();
+    }
+}
