4440cd94b867d5418fd719c51cf12c13f4f6f483
studentPun <s239053@students.aie.edu>
2023-10-04 11:38:00 -0700

    Merge branch 'main' into UI

--- Assets/Level-Gen/Scripts/StoreTile.cs
+++ Assets/Level-Gen/Scripts/StoreTile.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using items;
 
-namespace Level
+namespace LevelGen
 {
     public class StoreTile : MonoBehaviour
     {
         private Transform itemParent;
         [SerializeField] protected GameObject itemPrefab;
-        [SerializeField] protected itemCategory spawnCategory;
+        //[SerializeField] protected itemCategory spawnCategory;
         [SerializeField] protected bool randomCategory = false;
         public Vector3[] spawns = new Vector3[1] { Vector3.zero };
 
@@ -17,7 +18,7 @@ namespace Level
             if (randomCategory)
             {
                 //int num = Enum.GetNames(typeof(itemCategory)).Length;
-                spawnCategory = (itemCategory)Random.Range(0, 2);
+                //spawnCategory = (itemCategory)Random.Range(0, 2);
             }
             itemParent = new GameObject("ItemParent").transform;
             itemParent.SetParent(transform);
