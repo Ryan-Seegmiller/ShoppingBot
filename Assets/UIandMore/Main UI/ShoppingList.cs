13972134adc8077f364f1a452e757ba6ee848ef1
TBoneHolmes <s239054@students.aie.edu>
2023-10-09 17:16:43 -0700

    Uhhh fixed some thing I don't understand why or how it broke, plz save me
    How do I keep breaking this?

--- Assets/UIandMore/Main UI/ShoppingList.cs
+++ Assets/UIandMore/Main UI/ShoppingList.cs
@@ -82,37 +82,35 @@ public class ShoppingList : MonoBehaviour
     {
         for(int i = 0; i < displayItems.Length; i++)
         {
-            //int ignorer = 0;
-            //collected = false;
-
-            //Until further fixing, items will have to be collected in exact order
-            collected = GameManager.Instance.shoppingList[i] == GameManager.Instance.inventory[i];
-            //for (int j = 0; j < i; j++)
-            //{
-            //    ignorer = 0;
-            //    if (!striked[i])
-            //    {
-            //        //if they match, ignore that many in.
-            //        if (GameManager.Instance.inventory[i] == GameManager.Instance.inventory[j])
-            //        {
-            //                ignorer++;
-            //        }
-            //    }
-            //}
-            //for (int j = 0; j < GameManager.Instance.inventorySize; j++)
-            //{
-            //    if(GameManager.Instance.inventory[i] == GameManager.Instance.shoppingList[j])
-            //    {
-            //        if (ignorer > 0)
-            //        {
-            //            ignorer--;
-            //        }
-            //        else
-            //        {
-            //            collected = true;
-            //        }
-            //    }
-            //}
+            int ignorer = 0;
+            collected = false;
+            //collected = GameManager.Instance.shoppingList[i] == GameManager.Instance.inventory[i];
+            for (int j = 0; j < i; j++)
+            {
+                ignorer = 0;
+                if (!striked[i])
+                {
+                    //if they match, ignore that many in.
+                    if (GameManager.Instance.inventory[i] == GameManager.Instance.inventory[j])
+                    {
+                            ignorer++;
+                    }
+                }
+            }
+            for (int j = 0; j < GameManager.Instance.inventorySize; j++)
+            {
+                if(GameManager.Instance.inventory[i] == GameManager.Instance.shoppingList[j])
+                {
+                    if (ignorer > 0)
+                    {
+                        ignorer--;
+                    }
+                    else
+                    {
+                        collected = true;
+                    }
+                }
+            }
             //^^^Change out for if it's anywhere in the list and prevent double checking on same item
             string currentName = GameManager.Instance.ItemName(GameManager.Instance.shoppingList[i]);
             if(collected)
