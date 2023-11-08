5808526a322eea47c738350a398a27c7a9b8f065
CondorHalcon <judahhohawi@outlook.com>
2023-11-07 16:47:02 -0800

    Pause behaviours

--- Assets/GameManager/Scripts/GameManager.cs
+++ Assets/GameManager/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using enemymanager;
 using Items;
 using PlayerContoller;
+using static GameManager;
+using audio;
 
 //bug when adding reference to level gen assembly
 public class GameManager : MonoBehaviour, UIEvents
@@ -27,18 +29,9 @@ public class GameManager : MonoBehaviour, UIEvents
     public bool gameActive = false;
     public PlayerMovement player;
 
+    // TODO: elevator animation
     //public Transform elevatorPoint;
     public Animator elevatorAnim;
-    /*
-    public Vector3 starRatingsPerTime = new Vector3(180, 300, 500);//Time in seconds less than x for 3 star, between x and y for 2, more than y for 1, more than z for 0.
-    public int TimePenaltyPerMissedItemPerDollar = 15;
-    public float finalCalculatedTime = 0;
-    */
-    //GAME SCORING : 
-    // time<180=3star
-    // time>180 time < 500=2star
-    // time>500=1star
-    //EACH MISSED ITEM = X SECOND PENALTY. Possibly scale by price?
 
 
     #region UIEvents
@@ -51,11 +44,14 @@ public class GameManager : MonoBehaviour, UIEvents
     // TODO: pause/unpause functionality
     public void PauseGame()
     {
-        
+        ClockStop();
+        AudioManager.instance.PlaySound2D(0);
+        player.backupCameraCanvas.SetActive(false);
     }
     public void ContinueGame()
     {
-
+        ClockContinue();
+        player.backupCameraCanvas.SetActive(true);
     }
     public void StopGame()
     {
@@ -91,18 +87,20 @@ public class GameManager : MonoBehaviour, UIEvents
     public struct GameRules
     {
         public int waveCount;
-        public float wavePeriod;
-        public float gameStartTime; 
+        public float wavePeriod; // how often enemies will spawn
+        public float gameStartTime; // time the game started (may change if paused)
+        [HideInInspector] public float holdTime; // time saved when we pause
         public float gameTime
         {
             get { return Time.time - gameStartTime; }
             private set { }
         }
-        public GameRules(float time)
+        public GameRules(float holdTime)
         {
-            waveCount = 0;
-            wavePeriod = 0;
-            gameStartTime = time;
+            this.waveCount = 0;
+            this.wavePeriod = 0;
+            this.holdTime = holdTime;
+            this.gameStartTime = 0;
         }
     }
     private GameRules gameRules;
@@ -121,8 +119,8 @@ public class GameManager : MonoBehaviour, UIEvents
         EnemyManager.instance.player = player.gameObject;
 
         // clock
-        gameRules = new GameRules(Time.time);
-        StartCoroutine(Clock());
+        gameRules = new GameRules(0);
+        ClockStart();
         gameActive = true;
         gameRules.wavePeriod = 10;
     }
@@ -137,7 +135,7 @@ public class GameManager : MonoBehaviour, UIEvents
     }
     private void GameStop()
     {
-        StartCoroutine(Clock());
+        ClockStop();
         player.backupCameraCanvas.SetActive(false);
         ItemManager.instance.DestroyItems();
         EnemyManager.instance.DestroyEnemies();
@@ -146,12 +144,41 @@ public class GameManager : MonoBehaviour, UIEvents
     private void GameEnd()
     {
         // TODO: save score
-        StopCoroutine(Clock());
+        ClockStop();
         player.backupCameraCanvas.SetActive(false);
         UIChanger.instance.SetSceneScoring();
         ItemManager.instance.DestroyItems();
         EnemyManager.instance.DestroyEnemies();
         LevelGen.LevelManager.instance.DeleteLevel(false);
+    }
+    #endregion
+
+    #region Clock
+    protected void ClockStart()
+    {
+        gameRules.gameStartTime = Time.time;
+        gameRules.holdTime = 0;
+        StartCoroutine(Clock());
+    }
+    protected void ClockContinue()
+    {
+        gameRules.gameStartTime = Time.time - gameRules.holdTime;
+        gameRules.holdTime = 0;
+        StartCoroutine(Clock());
+    }
+    protected void ClockStop()
+    {
+        gameRules.holdTime = gameRules.gameTime;
+        StopCoroutine(Clock());
+    }
+    IEnumerator Clock()
+    {
+        while (true)
+        {
+            EnemyManager.instance.time = gameRules.gameTime;
+            TimeCalc.instance.timer = (int)(gameRules.gameTime * 100);
+            yield return new WaitForSeconds(.01f);
+        }
     }
     #endregion
 
@@ -165,63 +192,5 @@ public class GameManager : MonoBehaviour, UIEvents
     private void Update()
     {
         GameUpdate();
-    }
-
-    #region Coroutine
-    IEnumerator Clock()
-    {
-        while (true)
-        {
-            EnemyManager.instance.time = gameRules.gameTime;
-            TimeCalc.instance.timer = (int)(gameRules.gameTime * 100);
-            yield return new WaitForSeconds(.01f);
-        }
-    }
-    #endregion
-
-    //WHEN PLAYER ENTERS ELEVATOR TRIGGER COLLIDER
-    //code for player
-    void AttemptEndRound()
-    {/*
-        if (gameActive)
-        {
-            gameActive = false;
-            player.transform.position = elevatorPoint.position;
-            player.transform.rotation = Quaternion.identity;
-            player.SetActive(gameActive);
-            elevatorViewCamera.SetActive(!gameActive);
-            elevatorAnim.SetBool("state", gameActive);
-            //destroy all current enemies, clear list. Same for items
-            for (int i = 0; i < enemyManager.currentEnemies.Count; i++)
-                Destroy(enemyManager.currentEnemies[i].gameObject);
-            enemyManager.currentEnemies.Clear();
-            GameObject[] itemGOs = GameObject.FindGameObjectsWithTag("Item");
-            for (int i = 0; i < itemGOs.Length; i++)
-            {
-                Destroy(itemGOs[i].gameObject);
-            }
-            /*
-            //calculate sum of missed item penalties
-            float totalPenalty = 0;
-            for (int i = 0; i < ItemManager.instance.shoppingList.Length; i++)
-            {
-                //if the shopping list has an item that the inventory doesnt, add value*penalty to total penalty
-                if (!ItemManager.instance.inventory.Contains(ItemManager.instance.shoppingList[i]))
-                {   //cost of the item that is missing. shoppinglist[i] returns an int representing the item
-                    totalPenalty += ItemManager.instance.itemCost[ItemManager.instance.shoppingList[i]] * TimePenaltyPerMissedItemPerDollar;
-                }
-            }
-            finalCalculatedTime = GameTime + totalPenalty;
-            //Get number of "stars" earned for time
-            CurrentStars = 3;
-            if (finalCalculatedTime > starRatingsPerTime.x)
-                CurrentStars--;
-            if (finalCalculatedTime > starRatingsPerTime.y)
-                CurrentStars--;
-            if (finalCalculatedTime > starRatingsPerTime.z)
-                CurrentStars--;
-            
-
-        }*/
     }
 }
