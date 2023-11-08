cdab5c04123debb877701a635a1a5a984644f6b9
studentPun <s239053@students.aie.edu>
2023-11-07 16:52:26 -0800

    pause hotkey updated

--- Assets/UIandMore/Main UI/ControlsPopup.cs
+++ Assets/UIandMore/Main UI/ControlsPopup.cs
@@ -19,6 +19,8 @@ public class ControlsPopup : MonoBehaviour
     [SerializeField] GameObject theObject;
     [SerializeField] GameObject HotKeySymbol;
 
+    [SerializeField] string[] motivation = new string[5];
+
     void Start()
     {
         theObject.SetActive(false);
@@ -33,11 +35,33 @@ public class ControlsPopup : MonoBehaviour
             HotKeySymbol.SetActive(false);
             StartCoroutine(ShowControls());
         }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UIChanger.instance.SetPause();
+        }
     }
 
     public IEnumerator ShowControls()
     {
         for(int i = 0; i < sprites.Length; i++)
+        {
+            //yield return new WaitForSeconds(2);
+            img.sprite = sprites[i];
+            text.text = displayText[i];
+            desc.text = descString[i];
+            if (!theObject.activeSelf)
+            {
+                theObject.SetActive(true);
+            }
+            yield return new WaitForSeconds(2);
+        }
+        theObject.SetActive(false);
+        HotKeySymbol.SetActive(true);
+    }
+
+    public IEnumerator ShowMotivation()
+    {
+        for (int i = 0; i < sprites.Length; i++)
         {
             //yield return new WaitForSeconds(2);
             img.sprite = sprites[i];
