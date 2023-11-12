3ab153b12f916dcfcfb5087d6ff0311355a548e1
CondorHalcon <judahhohawi@outlook.com>
2023-11-11 20:41:25 -0800

    Refactored elevator doors

--- Assets/GameManager/Scripts/Elevator.cs
+++ Assets/GameManager/Scripts/Elevator.cs
@@ -10,6 +10,27 @@ public class Elevator : MonoBehaviour
     [SerializeField] Transform rightDoor;
     [SerializeField, Range(0, 1f)] float openPercent = 0;
 
+    internal void LockElevator()
+    {
+        lockCollider.enabled = true;
+    }
+    internal void UnlockElevator()
+    {
+        lockCollider.enabled = false;
+    }
+    private void UpdateElevatorDoors()
+    {
+        openPercent = Mathf.Clamp01(openPercent);
+        leftDoor.localPosition = new Vector3(Mathf.Lerp(0, 1, openPercent), 0, 0);
+        rightDoor.localPosition = new Vector3(Mathf.Lerp(0, -1, openPercent), 0, 0);
+    }
+
+    private void Update()
+    {
+        float direction = (lockCollider.enabled) ? -1 : 1;
+        openPercent = openPercent + (Time.deltaTime * direction);
+        UpdateElevatorDoors();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.root.GetComponentInChildren<PlayerMovement>())
@@ -24,28 +45,9 @@ public class Elevator : MonoBehaviour
             GameManager.instance.OnPlayerExitElevator();
         }
     }
-
-    internal void LockElevator()
-    {
-        lockCollider.enabled = true;
-    }
-    internal void UnlockElevator()
-    {
-        lockCollider.enabled = false;
-    }
-    private void Update()
-    {
-        float direction = (lockCollider.enabled) ? -1 : 1;
-        openPercent = openPercent + (Time.deltaTime * direction);
-        openPercent = Mathf.Clamp01(openPercent);
-        leftDoor.localPosition = new Vector3(Mathf.Lerp(0, 1, openPercent), 0, 0);
-        rightDoor.localPosition = new Vector3(Mathf.Lerp(0, -1, openPercent), 0, 0);
-    }
     private void OnValidate()
     {
         if (leftDoor == null || rightDoor == null) { Debug.LogWarning($"Elevator :: null reference to elevator doors.", this); return; }
-        openPercent = Mathf.Clamp01(openPercent);
-        leftDoor.localPosition = new Vector3(Mathf.Lerp(0, 1, openPercent), 0, 0);
-        rightDoor.localPosition = new Vector3(Mathf.Lerp(0, -1, openPercent), 0, 0);
+        UpdateElevatorDoors();
     }
 }
